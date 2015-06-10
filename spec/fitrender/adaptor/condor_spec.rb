3c445a31779d828383c287fc950538d4273f8907
Dominik Jancik <jancidom@fit.cvut.cz>
2015-06-10 12:19:51 +0200

    Tests for animated scens

--- spec/fitrender/adaptor/condor_spec.rb
+++ spec/fitrender/adaptor/condor_spec.rb
@@ -109,6 +109,76 @@ describe Fitrender::Adaptor::CondorShellAdaptor do
     end
   end
 
+  context 'animated scene submission' do
+    MAX_WAIT = 20
+
+    def spec_dir
+      File.dirname(File.absolute_path(__FILE__))
+    end
+
+    before :context do
+      scene = Fitrender::Adaptor::Scene.new(
+          renderer_id: 'Blender',
+          path: "#{spec_dir}/test_scene.blend",
+          options: { 'frames' => '1-3,4' }
+      )
+
+      puts SEPARATOR
+      puts 'Submitting scene'
+      puts SEPARATOR
+
+      @jobs = @adaptor.submit(scene)
+
+      puts 'Resulted in following jobs:'
+      @jobs.each do |job|
+        puts job
+      end
+
+      puts SEPARATOR
+    end
+
+    it 'has valid job ids' do
+      @jobs.each do |job|
+        expect(job[:id]).to match(/^[0-9]+$/)
+      end
+    end
+
+    it 'has a job for each frame' do
+      expect(@jobs.count).to eq(4)
+    end
+
+    it "solves the scene in up to #{MAX_WAIT} seconds" do
+      waiting_for = 0
+      all_completed = false
+
+      while waiting_for < MAX_WAIT
+        sleep 2
+        waiting_for += 2
+        all_completed = @jobs.all? do |job|
+          @adaptor.job_state(job[:id]).eql? Fitrender::Adaptor::States::JOB_STATE_COMPLETED
+        end
+        if all_completed
+          puts "Completed in ~#{waiting_for} seconds"
+          break
+        end
+      end
+
+      expect(all_completed).to eq(true)
+    end
+
+    it 'renders correctly' do
+      # Compare the finished render with a reference image, requires ImageMagick
+      @jobs.each do |job|
+        result_path = job[:path]
+        puts 'Comparing images with'
+        cmp_command = "compare -metric mae \"#{spec_dir}/test_result.png\" \"#{result_path}\" /tmp/diff.png"
+        puts cmp_command
+        cmp_result = `#{cmp_command}`
+        expect(cmp_result.to_i).to eq(0)
+      end
+    end
+  end
+
   it 'implements all methods' do
     unimplemented_methods = @adaptor.methods_to_implement
 
